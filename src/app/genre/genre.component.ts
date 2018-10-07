5b61c4438951a6dea65a43335ffb856045570e73
Elias Lebid <eliaslebid@gmail.com>
2018-10-08 00:50:40 +0300

    implement genre component

--- src/app/genre/genre.component.ts
+++ src/app/genre/genre.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../models/book';
+import { Observable } from 'rxjs/internal/Observable';
+import { BookService } from '../book.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
+import { Genre } from '../models/genre';
 
 @Component({
   selector: 'app-genre',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreComponent implements OnInit {
 
-  constructor() { }
+  books: Observable<Book[]>;
+  genre: Observable<Genre>;
+
+  constructor(private bookService: BookService,
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    const genreId = this.route.paramMap.pipe(
+      filter(x => x.has('genreId')),
+      map(x => Number(x.get('genreId'))),
+    );
+
+    this.genre = genreId.pipe(
+      switchMap(x => this.bookService.getGenreById(x))
+    );
+
+    this.books = genreId.pipe(
+      switchMap(x => this.bookService.getBooksByGenre(x))
+    );
   }
 
 }
